827d7ba742979dde1c4dc07025b6ed5d8f4768f2
JalenG20 <117691943+JalenG20@users.noreply.github.com>
2022-11-11 17:21:28 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,57 +1,143 @@
+//Jalen Gaddy            J00815158
 
-
-#include<iostream>
+#include <iostream>
 #include <string>
+
 using namespace std;
 
-int main()
-{
-    
-    int prime_num[11] = { 2,3,5,7,11,13,17,19,23,29,31 };
-    
-    int num, iter, c;
+void reset(int&, char&, string);
 
-    
-    cout << "Enter a positive number between 1 to 1000 "
-        << "Other than first 11 prime numbers\n";
-    cin >> num;
+int main() {
 
-   
-    c = 0;
+    int integer;
+    char choice;
 
-   
-    for (iter = 0; iter < 11; iter++) {
+START:
 
-       
-        if (num % prime_num[iter] != 0 || num == prime_num[iter]) {
+    cout << "Please input a positive integer between 1 and 1,000:  ";
 
-            //Increase the counter.
-            c++;
-        }
+    cin >> integer;
+
+    if (!cin)
+    {
+        reset(integer, choice, "\n Check input. Please try again.\n\n");
+        goto START;
     }
 
-    
-    if (c == 11)
+    else if (integer <= 1 || integer > 1000)
+    {
+        reset(integer, choice, "\n Integer must be between 1 and 1000. Please try again.\n\n");
+        goto START;
 
-        cout << num << " is Prime\n";
+    }
 
-  
+
+   
+    else if (integer == 2 || integer == 3 || integer == 5 || integer == 7 || integer == 11 || integer == 13 || integer == 17 || integer == 19 || integer == 23 || integer == 29 || integer == 31)
+
+    {
+        cout << "\n\n" << integer << " is prime.";
+        goto END;
+    }
+
+
+
+    else if ((integer % 2 == 0) || (integer % 3 == 0) || (integer % 5 == 0) || (integer % 7 == 0) || (integer % 11 == 0) || (integer % 13 == 0) || (integer % 17 == 0) || (integer % 19 == 0) || (integer % 23 == 0) || (integer % 29 == 0) || (integer % 31 == 0))
+
+    {
+        cout << "\n\n" << integer << " is not prime.\n\nIt is divisible by the following prime numbers: \n\n";
+    }
+
+
     else
     {
-       
-        cout << num << " is not prime and divisible by\n";
-        
-        for (iter = 0; iter < 11; iter++)
-        {
-           
-
-            if (num % prime_num[iter] == 0)
-                cout << prime_num[iter] << " ";
-
-        }
-        cout << "\n";
+        cout << integer << " is prime.\n\n\n";
     }
+
+
+    
+    if (integer % 2 == 0)
+    {
+        cout << 2 << " ";
+    }
+
+    if (integer % 3 == 0)
+    {
+        cout << 3 << " ";
+    }
+
+    if (integer % 5 == 0)
+    {
+        cout << 5 << " ";
+    }
+
+    if (integer % 7 == 0)
+    {
+        cout << 7 << " ";
+    }
+
+    if (integer % 11 == 0)
+    {
+        cout << 11 << " ";
+    }
+
+    if (integer % 13 == 0)
+    {
+        cout << 13 << " ";
+    }
+
+    if (integer % 17 == 0)
+    {
+        cout << 17 << " ";
+    }
+
+    if (integer % 19 == 0)
+    {
+        cout << 19 << " ";
+    }
+
+    if (integer % 23 == 0)
+    {
+        cout << 23 << " ";
+    }
+
+    if (integer % 29 == 0)
+    {
+        cout << 29 << " ";
+    }
+
+    if (integer % 31 == 0)
+    {
+        cout << 31 << " ";
+    }
+
+
+END:
+    cout << "\n\n\n\n";
+
+    cout << "\n\nWould you like to try again? ( y | n ): ";
+    cin >> choice;
+
+
+
+    if (toupper(choice) == 'Y')
+    { 
+        reset(integer, choice, "\n\nRESETTING PROGRAM\n\n");
+        goto START;
+    }
+
+
     return 0;
+}
+
+void reset(int& integer, char& choice, string msg)
+{ 
+    cin.clear();
+    cin.ignore(INT_MAX, '\n');
+    integer = NULL;
+    choice = NULL;
+    cout << msg;
 
 }
+
 
