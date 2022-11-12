058ff376a4a6908b40a2268997cf1deced11790a
JalenG20 <117691943+JalenG20@users.noreply.github.com>
2022-11-11 23:43:15 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,62 +1,114 @@
 //Jalen Gaddy            J00815158
 
-#include<iostream>
+
+#include <iostream>
 #include <string>
+
 using namespace std;
 
-int main()
-{
-    //Create the list of first 11 prime number.
-    int prime_num[11] = { 2,3,5,7,11,13,17,19,23,29,31 };
-    //Define the variables.
-    int num, iter, c;
+void reset(int&, char&, string);
 
-    //prompt user for input.
-    cout << "Enter a positive number between 1 to 1000 "
-        << "Other than first 11 prime numbers\n";
-    cin >> num;
-
-    //Set the counter variable.
-    c = 0;
-
-    //Use loop to check if number is prime.
-    for (iter = 0; iter < 11; iter++) {
-
-        //Check number divisibility by prime number list.
-        // need to check given number exist in the 1st 11 numbers
-        if (num % prime_num[iter] != 0 || num == prime_num[iter]) {
-
-            //Increase the counter.
-            c++;
-        }
+int main() {
+    
+    int integer;
+    char choice;
+    
+START:
+    
+    cout << "Please input a positive integer between 1 and 1,000:  ";
+    
+    cin >> integer;
+    
+    if (!cin)
+    {// Input validation. Handles input failure.
+        reset(integer, choice, "\n Check input. Please try again.\n\n");
+        goto START;
     }
+    
+    else if (integer <= 1 || integer > 1000)
+    {reset(integer, choice, "\n Integer must be between 1 and 1000. Please try again.\n\n");
+        goto START;
+        
+    }
+    
+    
+     //calculating to see if integer is prime. Notifying user of results.
+    else if (integer == 2 || integer == 3 || integer == 5 || integer == 7 || integer == 11 || integer == 13 || integer == 17 || integer == 19 || integer == 23 || integer == 29 || integer == 31)
+    
+    {cout << "\n\n" << integer << " is prime.";
+        goto END;}
 
-    //Display prime number.
-    if (c == 11)
 
-        cout << num << " is Prime\n";
-
-    //Else display the list of numbers that divides the
-    //entered number.
+   
+    else if((integer % 2 == 0) || (integer % 3 == 0) || (integer % 5 == 0) || (integer % 7 == 0) || (integer % 11 == 0) || (integer % 13 == 0) || (integer % 17 == 0) || (integer % 19 == 0) || (integer % 23 == 0) || (integer % 29 == 0) || (integer % 31 == 0))
+    
+    {cout << "\n\n" << integer << " is not prime.\n\nIt is divisible by the following prime numbers: \n\n";}
+    
+    
     else
-    {
-        //Display the message
-        cout << num << " is not prime and divisible by\n";
-        //use loop to find the list of number
-        //dividing the number input..
+    {cout << integer << " is prime.\n\n\n";}
+    
+    
+    //outputting prime numbers that can divide integer.
+    if (integer % 2 == 0)
+        {cout << 2<< " ";}
+    
+    if (integer % 3 == 0)
+        {cout << 3 << " ";}
+        
+    if (integer % 5 == 0)
+        {cout << 5<< " ";}
+        
+    if (integer % 7 == 0)
+        {cout << 7 << " ";}
+        
+    if (integer % 11 == 0)
+        {cout << 11 << " ";}
+        
+    if (integer % 13 == 0)
+        {cout << 13 << " ";}
+        
+    if (integer % 17 == 0)
+        {cout << 17 << " ";}
+        
+    if (integer % 19 == 0)
+        {cout << 19 << " ";}
+        
+    if (integer % 23 == 0)
+        {cout << 23 << " ";}
+        
+    if (integer % 29 == 0)
+        {cout << 29 << " ";}
+        
+    if (integer % 31 == 0)
+    {cout << 31 << " ";}
+    
 
-        for (iter = 0; iter < 11; iter++)
-        {
-            //Display the list of number dividing the number.
+END:
+    cout << "\n\n\n\n";
+    
+    cout << "\n\nWould you like to try again? ( y | n ): ";
+    cin >> choice;
 
-            if (num % prime_num[iter] == 0)
-                cout << prime_num[iter] << " ";
 
-        }
-        cout << "\n";
+
+     if (toupper(choice) == 'Y')
+    { // repeat program as necessary
+        reset(integer, choice,"\n\nRESETTING PROGRAM\n\n");
+        goto START;
     }
+    
+  
     return 0;
-
 }
 
+void reset(int &integer,char &choice,string msg)
+{ // clears values and notifies user of input validation
+    cin.clear();
+    cin.ignore(INT_MAX, '\n');
+    integer = NULL;
+    choice = NULL;
+    cout << msg;
+    
+}
 
